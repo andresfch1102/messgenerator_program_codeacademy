bcbaee4bde4c50dc9711759c8c27bf161f0d9926
andresfch1102 <123219293+andresfch1102@users.noreply.github.com>
2023-04-07 17:40:37 -0400

    implementation to form phrase

--- main.js
+++ main.js
@@ -6,12 +6,21 @@ const complementArr2= ['succeed','accomplish anything', 'see results', 'improve'
 const componentsArray = [subjects, verbs, noun, complementArr1, complementArr2];
 
 const messageGen = arr =>{
-    let motPhrase = '';
+    let motPhrase = [];
     let randArr = [];
     for(let i=0; i<arr.length; i++){
         let randI = Math.floor(Math.random() * arr[i].length);
         randArr.push(randI); 
      }
 
+     if(arr[0][randArr[0]] === 'Your'){
+        motPhrase.push(arr[0][randArr[0]],arr[2][randArr[2]],' will help you ', arr[4][randArr[4]]);
+     }else{
+        for(let i=0; i<randArr.length; i++){
+            motPhrase.push(arr[i][randArr[i]]);
+        }
+     }
+     return motPhrase.toString();
+
 }
-messageGen(componentsArray);
+console.log(messageGen(componentsArray));
